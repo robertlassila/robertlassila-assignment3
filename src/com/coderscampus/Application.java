344546c718c94624c91be9526fa65ef0c0da21a5
robertlassila <rlassila42@gmail.com>
2024-11-27 11:28:46 -0500

    1st Functioning Draft
    -As far as I can see, the app works
    -There is some tidying up that can be done for variable assignment and loops

--- src/com/coderscampus/Application.java
+++ src/com/coderscampus/Application.java
@@ -1,11 +1,64 @@
 package com.coderscampus;
 
+import java.util.Scanner;
+
 public class Application {
 
 	public static void main(String[] args) {
-
-		System.out.println(UserService.determineNumberOfUsers());
-
+		
+		//creates local variable with the number of users
+		int numberOfUsers = UserService.determineNumberOfUsers();
+		//System.out.println(numberOfUsers);
+		
+		//creates local array with all the users
+		User[] users = UserService.userInformation();
+		//System.out.println(users[1].getPassword());
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int loginAttempts = 0;
+		boolean loginSuccessful = false;
+		
+		while (loginAttempts < 5) {
+			
+			System.out.println("Enter your email: ");
+			String inputtedUsername = scanner.nextLine();
+			String lowerCaseInputtedUsername = inputtedUsername.toLowerCase();
+			
+			System.out.println("Enter your password: ");
+			String inputtedPassword = scanner.nextLine();
+			
+			
+			int index = 0;
+			while (index < numberOfUsers) {
+				String checkedUsername = users[index].getUsername();
+				String lowerCaseCheckedUsername = checkedUsername.toLowerCase();
+				String checkedPassword = users[index].getPassword();
+				
+				if (lowerCaseInputtedUsername.equals(lowerCaseCheckedUsername)) {
+					if (inputtedPassword.equals(checkedPassword)) {
+						System.out.println("Welcome " + users[index].getName());
+						loginSuccessful = true;
+						loginAttempts = 5;
+						break;
+					} else {
+						index++;
+					}
+				} else {
+					index++;
+				}
+			}
+			if (loginSuccessful == false) {
+				System.out.println("Invalid login, please try again");
+				loginAttempts++;	
+			}
+		}
+		if (loginSuccessful == false) {
+			System.out.println("Maximum login attempts reached.");
+		}
+		scanner.close();
+		
+		
 	}
 
 }
--- src/com/coderscampus/UserService.java
+++ src/com/coderscampus/UserService.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 
 public class UserService {
 
+	static String filepath = "data.txt";
 	public static int determineNumberOfUsers() {
 		String filepath = "data.txt";
 		int numberOfUsers = 0;
@@ -18,6 +19,37 @@ public class UserService {
 			return numberOfUsers;
 		}
 		return numberOfUsers; 
+		
 	}
+	
+	//this is creating the user array with the number of users
+	public static User[] users = new User[UserService.determineNumberOfUsers()];
+	
+	
+	//this method reads user information, splits it, stores it to a user, then stores each user to the User[]
+	public static User[] userInformation() {
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(filepath))){
+			String line;
+			int index = 0;
+			
+			while ((line = br.readLine()) != null) {
+				
+				String[] userDetails = line.split(",");
+				
+				String username = userDetails[0];
+				String password = userDetails[1];
+				String name = userDetails[2];
+				
+				users[index] = new User(username, password, name);
+				index++;
+			}
+			
+		} catch (Exception e) {
+			System.out.println("Error reading file; " + e.getMessage());
+		}
+		return users;
+	}
+	
 	
 }
